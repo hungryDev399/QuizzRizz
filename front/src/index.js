31717803744d4c8c5d428d1df817ae56dd969ab0
Youssef-Kalds <112778572+Youssef-Kalds@users.noreply.github.com>
2023-12-26 00:10:06 +0200

    solving front-end conflict

--- front/src/index.js
+++ front/src/index.js
@@ -1,34 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import './index.css';
 
-import App from "./App";
-import SignIn from "./signIn";
-import SignUp from "./signUp";
-import NavBar from "./components/NavBar";
-import Footer from "./components/footer";
+import App from './App';
+import SignIn from './signIn';
+import SignUp from './signUp';
+import Quizzes from './quizzes';
+import Quiz from './quiz';
+import NavBar from './components/NavBar';
+import Footer from './components/footer';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<NavBar />
-			<Routes>
-				<Route
-					path="/"
-					element={<App />}
-				/>
-				<Route
-					path="/signIn"
-					element={<SignIn />}
-				/>
-				<Route
-					path="/signUp"
-					element={<SignUp />}
-				/>
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/quizzes" element={<Quizzes />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>
 );
