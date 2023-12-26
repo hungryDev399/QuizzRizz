c45ea9da959cc0a603215539e19039f8910d40c2
Youssef-Kalds <112778572+Youssef-Kalds@users.noreply.github.com>
2023-12-26 22:38:01 +0200

    sign in page is done

--- front/src/components/Quiz.jsx
+++ front/src/components/Quiz.jsx
@@ -8,9 +8,9 @@ export default function Quiz({quizID, levelOfDifficulty, questionsNumber}) {
 			<section className="flex w-full max-w-[1216px] flex-col justify-center my-12 px-16 max-md:max-w-full max-md:my-10 max-md:px-5">
 				<div className="flex w-[768px] max-w-full flex-col items-center">
 					<div className="self-stretch text-white text-center text-5xl font-semibold leading-10 max-md:max-w-full">
-						<h3>Quiz {quizID}</h3>
-						<h3>level: {levelOfDifficulty}</h3>
-						<h3>number of questions {questionsNumber}</h3>
+						<h3 className="text-3xl">Quiz {quizID}</h3>
+						<h3 className="text-3xl">level: {levelOfDifficulty}</h3>
+						<h3 className="text-3xl">number of questions {questionsNumber}</h3>
 					</div>
 					<div className="items-stretch flex gap-4 mt-10  max-md:max-w-full max-md:flex-wrap"></div>
 				</div>
