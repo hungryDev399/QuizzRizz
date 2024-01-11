b3fea1c3369f99167843082b08206ce31f09fd80
hungryDev399 <mohammedabdelaziz399@gmail.com>
2024-01-11 08:17:39 +0200

    Changed the database to an azure hosted postgresql database

--- back/back/back/settings.py
+++ back/back/back/settings.py
@@ -87,11 +87,21 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'main',
+        'USER': 'mo',
+        'PASSWORD': 'admin',
+        'HOST': 'csai203mainserver.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
